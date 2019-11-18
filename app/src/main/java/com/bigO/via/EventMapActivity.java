a6038666e1f89f15fd38019be460cfa8563cb114
Masroor-Syed <suroor.23@gmail.com>
2019-11-18 16:28:54 -0700

    bottom navbar for and second activity

---
+++ app/src/main/java/com/bigO/via/EventMapActivity.java
@@ -0,0 +1,14 @@
+package com.bigO.via;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_map);
+    }
+}
--- app/src/main/java/com/bigO/via/HomeFragment.java
+++ app/src/main/java/com/bigO/via/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.bigO.via;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -13,6 +16,29 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ImageButton search_button = (ImageButton) view.findViewById(R.id.search_button);
+
+        search_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) { searchHandler(v); }
+        });
+
+        return view;
     }
+
+    /** Called when the user touches the search button */
+    public void searchHandler(View view) {
+        // Do something in response to search click
+        Intent eventMapIntend;
+        eventMapIntend = new Intent(HomeFragment.this.getActivity(), EventMapActivity.class);
+        this.startActivity(eventMapIntend)
+         ;
+    }
+
+
+
+
 }
