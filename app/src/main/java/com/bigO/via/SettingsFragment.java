198dcbb5a241e8287e259bb8e394f6a97b137dc1
HAMZAHSPC\Hamzah <hamzah.umar@ucalgary.ca>
2019-11-26 14:12:37 -0700

    Settings page implemented
    - TODO: Add functionality

--- app/src/main/java/com/bigO/via/SettingsFragment.java
+++ app/src/main/java/com/bigO/via/SettingsFragment.java
@@ -8,11 +8,17 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends androidx.preference.PreferenceFragmentCompat {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.settings);
     }
 }
